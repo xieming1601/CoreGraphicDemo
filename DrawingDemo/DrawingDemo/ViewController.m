95f9a734a619ad5285ba633777f0106380649c3b
xsm <xieming1601@163.com>
2016-09-20 10:36:15 +0800

    无

--- DrawingDemo/DrawingDemo/ViewController.m
+++ DrawingDemo/DrawingDemo/ViewController.m
@@ -14,31 +14,29 @@
 
 @implementation ViewController
 
-@synthesize myView;
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    myView = [[CostomView alloc] init];
+    _myView = [[CostomView alloc] init];
     
-    [self.view addSubview:myView];
+    [self.view addSubview:_myView];
     
-    [myView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_myView setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     //子view的上边缘离父view的上边缘40个像素
-    NSLayoutConstraint *contraint1 = [NSLayoutConstraint constraintWithItem:myView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:10.0];
+    NSLayoutConstraint *contraint1 = [NSLayoutConstraint constraintWithItem:_myView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:10.0];
     
     //子view的左边缘离父view的左边缘40个像素
     
-    NSLayoutConstraint *contraint2 = [NSLayoutConstraint constraintWithItem:myView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:10.0];
+    NSLayoutConstraint *contraint2 = [NSLayoutConstraint constraintWithItem:_myView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:10.0];
     
     //子view的下边缘离父view的下边缘40个像素
     
-    NSLayoutConstraint *contraint3 = [NSLayoutConstraint constraintWithItem:myView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-10.0];
+    NSLayoutConstraint *contraint3 = [NSLayoutConstraint constraintWithItem:_myView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-10.0];
     
     //子view的右边缘离父view的右边缘40个像素
     
-    NSLayoutConstraint *contraint4 = [NSLayoutConstraint constraintWithItem:myView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.0 constant:-10.0];
+    NSLayoutConstraint *contraint4 = [NSLayoutConstraint constraintWithItem:_myView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.0 constant:-10.0];
     
     //把约束添加到父视图上
     
